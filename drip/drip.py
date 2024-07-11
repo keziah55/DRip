64a21a82fad619436d49d611d62bd86334ec3ec2
keziah55 <19816994+keziah55@users.noreply.github.com>
2024-07-11 21:49:09 +0100

    icons

--- drip/drip.py
+++ drip/drip.py
@@ -1,6 +1,13 @@
+from pathlib import Path
 from qtpy.QtWidgets import QTabWidget
+from qtpy.QtGui import QIcon
 from .dvdbackupwidget import DvdBackupWidget
 from .ffmpegwidget import FfmpegWidget
+
+def get_path():
+    p = Path(__file__)
+    p = p.resolve()
+    return p.parent.parent
 
 class DRip(QTabWidget):
     def __init__(self):
@@ -14,5 +21,7 @@ class DRip(QTabWidget):
         
         self.dvdbackup.vobPathChanged.connect(self.ffmpeg.vobPathChanged)
     
-            
+        p = get_path().joinpath('images').joinpath('icon.png')
+        icon = QIcon(str(p))
+        self.setWindowIcon(icon)
         
--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 Run Cycle Tracks.
 """
 import sys
+from pathlib import Path
 from qtpy.QtWidgets import QApplication
 from drip.drip import DRip
 
@@ -13,6 +14,11 @@ if __name__ == '__main__':
     QApplication.setOrganizationName("Tracks")
     
     app = QApplication(sys.argv)
+    # set desktop file, if it exists
+    # this allows the correct icon to be shown on wayland
+    p = Path.home().joinpath(".local", "share", "applications", "drip.desktop")
+    if p.exists():
+        app.setDesktopFileName(str(p))
     
     window = DRip()
     window.show()
